33b9a938b37f6eefa1694d3a65f7b680d560e461
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-02-05 14:49:26 -0600

    adds Page js

---
+++ my-app/src/components/Page/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PageContent from "../PageContent";
+import Home from "../Home";
+import About from "../About";
+import Portfolio from "../Portfolio";
+import Contact from "../Contact";
+import Resume from "../Resume";
+
+function Page({ currentPage }) {
+    const renderPage = () => {
+        switch (currentPage.name) {
+            case 'home':
+                return <Home />;
+            case 'about':
+                return <About />;
+            case 'portfolio':
+                return <Portfolio />;
+            case 'contact':
+                return <Contact />;
+            case 'resume':
+                return <Resume />;
+            default:
+                return <Home />;
+        }
+    };
+
+    return (
+        <section>
+            <h2>{(currentPage.name)}</h2>
+            <PageContent>{renderPage()}</PageContent>
+        </section>
+    );
+}
+export default Page;
