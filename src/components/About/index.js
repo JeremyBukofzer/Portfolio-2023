48a0afd4a479b48f39b90cef30cc2cb4c6f85719
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-04-15 13:06:41 -0500

    starts about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <section className="about-container">
             <div className="about-text">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices leo tellus, sit amet dictum lacus aliquam in. Suspendisse ut metus ut mauris sagittis eleifend. In eleifend suscipit dolor quis aliquet. Quisque feugiat odio sed ex varius, sed commodo neque tincidunt. Pellentesque condimentum pharetra aliquam. Proin iaculis auctor viverra. Morbi euismod posuere nulla, vel gravida nisl consequat at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut lorem sem. Donec ornare mattis hendrerit. Donec eget feugiat neque. Maecenas sit amet tortor non mi tincidunt pharetra sed commodo felis. Pellentesque condimentum ut est at iaculis. Vestibulum elementum ipsum sed sapien volutpat scelerisque. Curabitur congue scelerisque sem quis lobortis. In nec placerat felis
+                <p>I am a Full stack Developer who is eager to share my passion for creativity and problem solving. I have been educated on front end and back end programmig, with a passion for front end design.
                 </p>
             </div>
 
