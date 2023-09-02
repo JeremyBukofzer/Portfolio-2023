db0c7389e30bb3191fbd9d30341a826bfa8851a2
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-09-02 17:05:56 -0500

    updates card structure

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,44 +1,55 @@
 import React from "react";
 import arthubLogo from "./Images/art-hub-logo-ticket.png"
-import encyclonLogo from "./Images/encyclone-logo.png"
+import encycloneLogo from "./Images/encyclone-logo.png"
 
 
 function Projects() {
 
     return (
-        <div className="project-container">
-            <div class="arthub-card">
-                <img className="arthub-logo" src={arthubLogo} alt="arthub-logo" />
-                <div className="card-content">
-                    <h2>
-                        Card Heading
-                    </h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
-                    </p>
-                    <a href="https://ddwk21.github.io/art-hub/" className="button">
-                        Find out more
-                        <span class="material-symbols-outlined">
-                            arrow_right_alt
-                        </span>
-                    </a>
+        <div>
+            <h1 className="project-title">My Projects</h1>
+            <div className="project-container">
+                <div className="card">
+                    <img className="logo" src={arthubLogo} alt="arthub-logo" />
+                    <div className="card-content">
+                        <h2>
+                            Art Hub
+                        </h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
+                        </p>
+                        <a href="https://ddwk21.github.io/art-hub/" className="button">
+                            Find out more
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className="encyclone-card">
-                <img className="encyclone-logo" src={encyclonLogo} alt="encyclone-logo" />
-                <div className="card-content">
-                    <h2>
-                        Card Heading
-                    </h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
-                    </p>
-                    <a href="https://fierce-sands-41595.herokuapp.com/" className="button">
-                        Find out more
-                        <span className="material-symbols-outlined">
-                            arrow_right_alt
-                        </span>
-                    </a>
+                <div className="card">
+                    <img className="logo" src={encycloneLogo} alt="encyclone-logo" />
+                    <div className="card-content">
+                        <h2>
+                            Card Heading
+                        </h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
+                        </p>
+                        <a href="https://fierce-sands-41595.herokuapp.com/" className="button">
+                            Find out more
+                        </a>
+                    </div>
+                </div>
+                <div className="card">
+                    <img className="logo" src={encycloneLogo} alt="weather-logo" />
+                    <div className="card-content">
+                        <h2>
+                            Card Heading
+                        </h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
+                        </p>
+                        <a href="https://fierce-sands-41595.herokuapp.com/" className="button">
+                            Find out more
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
